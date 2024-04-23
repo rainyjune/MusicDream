02964858e915144c0cacef73f5af7a4a310103db
rainyjune <rainyjune@live.cn>
2024-04-23 23:01:55 +0800

    Use modern meta tag

--- protected/views/layouts/main.php
+++ protected/views/layouts/main.php
@@ -2,7 +2,7 @@
   "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
 <head>
-  <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+    <meta charset="utf-8" />
   <meta name="viewport" content="width=device-width, initial-scale=1" />
 
   <!-- blueprint CSS framework -->
